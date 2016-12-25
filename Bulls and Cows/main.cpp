cea66d56852d1b625fce6ca3d1c730baed664f73
Patrick Lockett <patricklockett@patricks-macbook-pro.local>
2016-12-25 09:47:33 -0800

    Initial Commit

---
+++ Bulls and Cows/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bulls and Cows
+//
+//  Created by Patrick Lockett on 12/25/16.
+//  Copyright © 2016 Patrick Lockett. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
